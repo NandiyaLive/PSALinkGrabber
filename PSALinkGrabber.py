bfddc78fe6527159816d528ca00a2f52dd05df11
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2020-12-01 08:10:11 +0530

    Add files via upload

---
+++ PSALinkGrabber.py
@@ -0,0 +1,45 @@
+import cloudscraper as cs
+from bs4 import BeautifulSoup as bs
+
+
+url = input("PSA Page Link : ")
+
+quality = input("1) 720p | 2) 1080p\nEnter a number: ")
+
+scraper = cs.create_scraper()
+r = scraper.get(url).text
+soup = bs(r, "lxml")
+fulldivs = soup.findAll("div", class_="sp-wrap sp-wrap-steelblue")
+
+data = {}
+
+for fulldiv in fulldivs:
+    if quality == 1:
+        if "720p" in fulldiv.text.lower():
+            div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
+            title = fulldiv.find("div", class_="sp-head").text.replace("\n", "")
+            link = div.find("a")["href"]
+            data["title"] = title
+            data["link"] = link
+
+            print(data)
+
+    elif quality == 2:
+        if "1080p" in fulldiv.text.lower():
+            div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
+            title = fulldiv.find("div", class_="sp-head").text.replace("\n", "")
+            link = div.find("a")["href"]
+            data["title"] = title
+            data["link"] = link
+
+            print(data)
+    else:
+        if "720p" in fulldiv.text.lower():
+            div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
+            title = fulldiv.find(
+                "div", class_="sp-head").text.replace("\n", "")
+            link = div.find("a")["href"]
+            data["title"] = title
+            data["link"] = link
+
+            print(data)
