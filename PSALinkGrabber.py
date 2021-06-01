18313c8f46e407d8dd6548f717b01f43d6bace49
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2021-06-01 21:27:43 +0530

    Update PSALinkGrabber.py

--- PSALinkGrabber.py
+++ PSALinkGrabber.py
@@ -2,47 +2,54 @@
 # This program is dedicated to the public domain under the CC0 license.
 # Coded with ❤️ by Neranjana Prasad (@NandiyaLive)
 
-import cloudscraper as cs
-from bs4 import BeautifulSoup as bs
+import subprocess
 
+def install():
+    subprocess.call(['pip', 'install', "bs4", "lxml", "requests", "cloudscraper"])
+    
+    getdata()
 
-url = input("PSA Page Link : ")
-quality = input("1) 720p | 2) 1080p\nEnter a number: ")
+def getdata():
+    import cloudscraper as cs
+    from bs4 import BeautifulSoup as bs
+    
+    url = input("PSA Page Link : ")
+    quality = input("1) 720p | 2) 1080p\nEnter a number: ")
 
-scraper = cs.create_scraper()
-r = scraper.get(url).text
-soup = bs(r, "lxml")
-fulldivs = soup.findAll("div", class_="sp-wrap sp-wrap-steelblue")
+    scraper = cs.create_scraper()
+    r = scraper.get(url).text
+    soup = bs(r, "lxml")
+    fulldivs = soup.findAll("div", class_="sp-wrap sp-wrap-steelblue")
 
-data = {}
+    data = {}
 
-for fulldiv in fulldivs:
-    if quality == 1:
-        if "720p" in fulldiv.text.lower():
-            div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
-            title = fulldiv.find("div", class_="sp-head").text.replace("\n", "")
-            link = div.find("a")["href"]
-            data["title"] = title
-            data["link"] = link
+    for fulldiv in fulldivs:
+        if quality == 1:
+            if "720p" in fulldiv.text.lower():
+                div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
+                title = fulldiv.find("div", class_="sp-head").text.replace("\n", "")
+                link = div.find("a")["href"]
+                data["title"] = title
+                data["link"] = link
 
-            print(data)
+                print(data)
 
-    elif quality == 2:
-        if "1080p" in fulldiv.text.lower():
-            div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
-            title = fulldiv.find("div", class_="sp-head").text.replace("\n", "")
-            link = div.find("a")["href"]
-            data["title"] = title
-            data["link"] = link
+        elif quality == 2:
+            if "1080p" in fulldiv.text.lower():
+                div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
+                title = fulldiv.find("div", class_="sp-head").text.replace("\n", "")
+                link = div.find("a")["href"]
+                data["title"] = title
+                data["link"] = link
 
-            print(data)
-    else:
-        if "720p" in fulldiv.text.lower():
-            div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
-            title = fulldiv.find(
-                "div", class_="sp-head").text.replace("\n", "")
-            link = div.find("a")["href"]
-            data["title"] = title
-            data["link"] = link
+                print(data)
+        else:
+            if "720p" in fulldiv.text.lower():
+                div = fulldiv.find("div", class_="dropshadowboxes-drop-shadow")
+                title = fulldiv.find(
+                    "div", class_="sp-head").text.replace("\n", "")
+                link = div.find("a")["href"]
+                data["title"] = title
+                data["link"] = link
 
-            print(data)
+                print(data)
